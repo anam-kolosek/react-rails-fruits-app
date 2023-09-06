7d0e5a993036dd25bdaa2d4c1c69665b434af23c
Aleksandar <aleksandar.zoric@ext.webedia-group.com>
2023-09-06 09:03:28 +0200

    Edit and delete comments

--- app/assets/javascripts/components/comments/_comment.js.jsx
+++ app/assets/javascripts/components/comments/_comment.js.jsx
@@ -1,16 +1,52 @@
-const Comment = ({comment}) => {
+class Comment extends React.Component {
 
-  const commentStyle = {
-    width: '20%',
-    marginBottom: '1%',
-    padding: '1%',
-    borderRadius: '5px',
-    border: '1px solid gray'
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editMode: props.editMode || false
+    };
   }
 
-  return (
-    <div style={commentStyle}>
-      {comment.body}
-    </div>
-  )
+  changeEditMode() {
+    this.setState({
+      editMode: !this.state.editMode
+    })
+  }
+
+  render() {
+    const commentStyle = {
+      width: '20%',
+      marginBottom: '0.5%',
+      padding: '1%',
+      paddingBottom: '0.5%',
+      borderRadius: '5px',
+      border: '1px solid gray'
+    }
+  
+    const editBtnStyle = {
+      marginRight: '3px'
+    }
+
+    const { comment, handleEdit, handleDelete } = this.props;
+    let formFields = { }
+
+    if (!this.state.editMode)
+      return(
+        <div style={commentStyle}>
+          <div>{comment.body}</div>
+          <br/>
+          <button onClick={() => this.changeEditMode()} className="btn btn-success btn-xs" style={editBtnStyle}>Edit</button>
+          <button onClick={() => {if(window.confirm('Delete the comment?')){handleDelete(comment.id)};}} className="btn btn-danger btn-xs">Delete</button>
+        </div>
+      )
+    return (
+      <div style={commentStyle}>
+        <textarea rows='5' ref={input => formFields.body = input} defaultValue={comment.body} className="form-control" />
+        <br/>
+        <button onClick={() => handleEdit(comment, formFields.body.value)} className="btn btn-success btn-xs" style={editBtnStyle}>Save</button>
+        <button onClick={() => this.changeEditMode()} className="btn btn-default btn-xs">Cancel</button>
+      </div>
+    )
+  }
 }
